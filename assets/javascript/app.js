6d536b5c2e09e148e7042651c31f8d07039d98c1
Lucio Verane <verane64@gmail.com>
2019-10-01 00:59:48 -0500

    js to add favorites added, basic css spacing added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,6 @@ var animals = ["cat", "dog", "sheep", "bunny", "wolf", "racoon"];
 
 var position = 0;
 
-
-
 function renderButton() {
     for (i = position; i < animals.length; i++) {
         animal = animals[i].charAt(0).toUpperCase() + animals[i].slice(1);
@@ -70,58 +68,80 @@ function animateGif() {
     }
 };
 
-// var favorites = [];
-// var favPosition = 0;
-var test = [];
-// localStorage.favorites[i] //use .length to loop trough it
+function onLoad() {
+    if (localStorage.favLink === undefined) {
+        favLink = [];
+        console.log("is undefined")
+    } else {
+        favLink = JSON.parse(localStorage.favLink)
+        console.log("is defined")
+    }
 
-function makeFavorite() {//save eahc one as an object
-    // console.log($(this).attr("data-still"))
-    // var favorite = {favStill : $(this).attr("data-still"), favAnimate : $(this).attr("data-animate")}
-    // console.log(favLink)
-    favLink = $(this).attr("data-link")
-    favPosition = $(this).attr("data-position")
-    var favorite = {link : favLink, position : favPosition}
-    // var favorite = { favLink: $(this).attr("data-link"), favPosition: $(this).attr("data-position") }
-    console.log("favorite="+favorite.link+favorite.position)
-    // favorites.push(favorite)
-    // console.log("favorites= " + favorites)
-    // localStorage.setItem('firstFavorite', JSON.stringify(favorite))
-    // localStorage.favorites = favorites
-    // console.log("local.favorites= " + localStorage)
-    test.push({link : favLink, position : favPosition})//other option
+    if (localStorage.favPosition === undefined) {
+        favPosition = [];
+    } else {
+        favPosition = JSON.parse(localStorage.favPosition)
+    }
 }
 
+onLoad();
+
+function makeFavorite() {
+
+    favLink.push($(this).attr("data-link"))
+    favPosition.push($(this).attr("data-position"))
+
+    console.log(favLink)
+    console.log(favPosition)
+
+    localStorage.favLink = JSON.stringify(favLink)
+    localStorage.favPosition = JSON.stringify(favPosition)
+    console.log(localStorage.favLink)
+    console.log(localStorage.favPosition)
+
+    console.log("length" + favLink.length)
+    console.log("length" + favPosition.length)
+
+    loadFavorite()
+}
+
+var j = 0;
+
+//maybe allow hide favorites?
+//need a proper on document ready function
+//need a clear favorite/localStorage function/button
 function loadFavorite() {
-    console.log(localStorage.favorites)
-    // for (var j = 0; j < localStorage.favorites.length; j++) {
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // }).then(function (response) {
-        //     for (i = 0; i < response.data.length; i++) {
-        //         console.log(response.data[i])
-    
-        //         var newDiv = $(`<div class=show-gif>`);
-        //         var favButton = $(`<button class=fav data-link=${queryURL} data-position=${i}>Mark Favourite</button>`)
-        //         var newImg = $(`<img data-state=still class=gif>`)
-        //         var newRating = $("<p>").text(`Rating: ${response.data[i].rating}`);
-        //         var newTitle = $("<p>").text(`Title: ${response.data[i].title}`);
-    
-        //         newImg.attr("data-animate", response.data[i].images.fixed_height.url)
-        //         newImg.attr("data-still", response.data[i].images.fixed_height_still.url)
-        //         newImg.attr("src", response.data[i].images.fixed_height_still.url);
-    
-        //         newDiv.prepend(newImg, newTitle, newRating, favButton);
-        //         $("#animalGifs").prepend(newDiv);
-    
-    
-        //     }
-        // });
+    // for(j=0; j<favLink.length; j++){
+    if (j < favLink.length) {
+        var queryURL = favLink[j]
+        console.log(queryURL)
+        var i = favPosition[j]
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+
+            var newDiv = $(`<div class=show-gif>`);
+            // var favButton = $(`<button class=fav data-link=${queryURL} data-position=${i}>Mark Favourite</button>`)
+            var newImg = $(`<img data-state=still class=gif>`)
+            var newRating = $("<p>").text(`Rating: ${response.data[i].rating}`);
+            var newTitle = $("<p>").text(`Title: ${response.data[i].title}`);
+
+            newImg.attr("data-animate", response.data[i].images.fixed_height.url)
+            newImg.attr("data-still", response.data[i].images.fixed_height_still.url)
+            newImg.attr("src", response.data[i].images.fixed_height_still.url);
+
+            newDiv.prepend(newImg, newTitle, newRating);
+            $("#favorites").prepend(newDiv);
+            j++
+            loadFavorite();
+        });
+    }
     // }
 }
 
-// loadFavorite()
+loadFavorite()
 
 $(document).on("click", ".fav", makeFavorite);
 
