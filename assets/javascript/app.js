33b149b6dd67a2d01e444a890384189ab50f0993
Lucio Verane <verane64@gmail.com>
2019-10-06 21:57:37 -0500

    gif result randomized, favorite bar hidden when empty, user can toggle show/hide favorites, automatic gifs search on button added by user, css improved

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,44 @@
 var animals = ["cat", "dog", "sheep", "bunny", "wolf", "racoon"];
 
 var position = 0;
+var newAnimal;
 
 function renderButton() {
     for (i = position; i < animals.length; i++) {
         animal = animals[i].charAt(0).toUpperCase() + animals[i].slice(1);
-        // console.log(animal);
         $("#buttons-view").append($(`<button class=animal-btn data-name=${animal}>${animal}</button>`));
         position++;
     }
 }
 
 $("#add-gif").on("click", function (event) {
-
     event.preventDefault();
 
     newAnimal = $("#gif-input").val().trim();
-    // console.log(newAnimal);
-
     animals.push(newAnimal);
 
     $("#gif-input").val("");
 
     renderButton();
+    displayGif();
 });
 
 
 function displayGif() {
+    $("#gif-result").attr("hidden", false)
     var gif = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=EOZ19rTF7jEoLoW62HdEMAhgfmg0Ajlc&limit=10&q=" + gif;
+    if(gif === undefined){
+        gif = newAnimal;
+    }
+    offset = Math.floor(Math.random() * 100)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=EOZ19rTF7jEoLoW62HdEMAhgfmg0Ajlc&limit=10&q=" + gif + "&offset=" + offset;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         var arr = response.data;
-        arr.forEach(function (gif, i) {
-
-            // console.log(gif)
+        arr.forEach(function (gif) {
 
             var newDiv = $(`<div class=show-gif>`);
             var favButton = $(`<button class=fav data-animated=${gif.images.fixed_height.url} data-still=${gif.images.fixed_height_still.url} data-rating=${gif.rating} data-title=${gif.title}>Mark Favourite</button>`)
@@ -69,14 +70,11 @@ function animateGif() {
 };
 
 var favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
-// console.log(favorites)
 
 function makeFavorite() {
 
     var userFavs = { favAnimated: $(this).attr("data-animated"), favStill: $(this).attr("data-still"), favTitle: $(this).attr("data-title"), favRating: $(this).attr("data-rating") }
-    // console.log("userFavs = " + JSON.stringify(userFavs));
     favorites.push(userFavs);
-    // console.log("favorites = " + JSON.stringify(favorites));
 
     localStorage.setItem("favorites", JSON.stringify(favorites));
 
@@ -86,33 +84,49 @@ function makeFavorite() {
 var globalCount = 0;
 
 function loadFavorite() {
-    var arr = JSON.parse(localStorage.favorites);
-    
-    //needed a global count for it to work and not generate all favorites every time a new one is added
-    for(i=globalCount; i<arr.length; i++){
-    // arr.forEach(function (userFavs, i) {
+    if (localStorage.favorites === undefined) {
+        $("#fav-box").attr("hidden", true)
+    } else {
+        $("#fav-box").attr("hidden", false)
+        var arr = JSON.parse(localStorage.favorites);
 
-        var newDiv = $(`<div class=show-gif>`);
-        var newImg = $(`<img data-state=still class=gif>`)
-        var newRating = $("<p>").text(`Rating: ${arr[i].favRating}`);
-        var newTitle = $("<p>").text(`Title: ${arr[i].favTitle}`);
+        //needed a global count for it to work and not generate all favorites every time a new one is added
+        for (i = globalCount; i < arr.length; i++) {
 
-        newImg.attr("data-animate", arr[i].favAnimated)
-        newImg.attr("data-still", arr[i].favStill)
-        newImg.attr("src", arr[i].favStill);
+            var newDiv = $(`<div class=show-gif>`);
+            var newImg = $(`<img data-state=still class=gif>`)
+            var newRating = $("<p>").text(`Rating: ${arr[i].favRating}`);
+            var newTitle = $("<p>").text(`Title: ${arr[i].favTitle}`);
 
-        newDiv.prepend(newImg, newTitle, newRating);
-        $("#favorites").prepend(newDiv);
-    // })
-    globalCount++;
+            newImg.attr("data-animate", arr[i].favAnimated)
+            newImg.attr("data-still", arr[i].favStill)
+            newImg.attr("src", arr[i].favStill);
+
+            newDiv.prepend(newImg, newTitle, newRating);
+            $("#favorites").prepend(newDiv);
+
+            globalCount++;
+        }
     }
-    
+
 }
 
-$("#clearFavs").on("click", function () {
+$("#clear-favs").on("click", function () {
     localStorage.clear();
     $("#favorites").html("");
-    // console.log(localStorage);
+    $("#fav-box").attr("hidden", true)
+});
+
+$("#show-favs").on("click", function () {
+    $("#hide-favs").attr("hidden", false)
+    $("#show-favs").attr("hidden", true)
+    $("#favorites").attr("hidden", false)
+});
+
+$("#hide-favs").on("click", function () {
+    $("#hide-favs").attr("hidden", true)
+    $("#show-favs").attr("hidden", false)
+    $("#favorites").attr("hidden", true)
 });
 
 $(document).on("click", ".fav", makeFavorite);
